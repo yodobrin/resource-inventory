79834fc1dcca237f414c128751b40fb9832aaefc
Yoav Dobrin <yodobrin@microsoft.com>
2024-09-01 12:02:28 +0300

    single $value output

--- GatewayFunctionBase.cs
+++ GatewayFunctionBase.cs
@@ -12,15 +12,28 @@ public abstract class GatewayFunctionBase : IGatewayFunction
     public virtual string MergeResults(List<string> jsonResponses)
     {
         var allResults = new List<JsonElement>();
+
         foreach (var jsonResponse in jsonResponses)
         {
             var jsonDocument = JsonDocument.Parse(jsonResponse);
-            allResults.Add(jsonDocument.RootElement.Clone());
+            var rootElement = jsonDocument.RootElement;
+
+            // Check if the root element contains a "value" property
+            if (rootElement.TryGetProperty("value", out var valueElement))
+            {
+                // Add the contents of the "value" array to the results
+                allResults.AddRange(valueElement.EnumerateArray());
+            }
+            else
+            {
+                // If there's no "value" array, add the root element itself
+                allResults.Add(rootElement);
+            }
         }
+
         var options = new JsonSerializerOptions { WriteIndented = true };
         return JsonSerializer.Serialize(new { value = allResults }, options);
     }
-
     // Abstract methods that derived classes must implement
     public abstract bool ValidateInputs(HttpRequest req, out string validationError);
     public abstract string BuildRequestUrl(string baseUrl, string identifier);
